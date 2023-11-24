d1437e175f7c7813149424d1fc198da1dffb9ab3
1lxrd <yashnik.dmitriu@gmail.com>
2023-11-25 00:20:14 +0200

    First homework

---
+++ hmw1.js
@@ -0,0 +1,26 @@
+"use strict"
+
+function drawTriangle(count, symbol = '+') {
+    var result = '';
+    for (let i=1; i<=count; i++){
+        for (let j=0; j<i; j++){
+            result += symbol+" ";
+        }
+        result +='\n';
+    }
+    console.log(result);
+}
+
+function drawTriangleAlternative(count, symbol='+'){
+    var result ='';
+    let i = 0;
+
+    while (i<count){
+        result +=symbol + " ";
+        console.log(result);
+        i++;
+    }
+}
+
+drawTriangle(6)
+drawTriangleAlternative(6, '*')
