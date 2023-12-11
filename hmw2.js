ab9a8a7104cbdbc22e252e40f4b11325f062e0fa
1lxrd <yashnik.dmitriu@gmail.com>
2023-12-11 17:16:32 +0200

    Second hmw

---
+++ hmw2.js
@@ -0,0 +1,63 @@
+const company = {
+    name: 'Велика Компанія',
+    type:'Головна компанія',
+    platform: 'Платформа для продажу квитків',
+    sellsSolution: 'Рішення для продажу квитків',
+    clients: [
+        {
+            name: 'Клієнт 1',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+            partners: [
+                {
+                    name: 'Клієнт 1.1',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                },
+                {
+                    name: 'Клієнт 1.2',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                    partners: [
+                        {
+                            name: 'Клієнт 1.2.1',
+                            type: 'subSubCompany',
+                            uses: 'Рішення для продажу квитків',
+                            sells: 'Рішення для продажу квитків',
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Клієнт 2',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків'
+        }
+    ]
+};
+
+
+var findValueByKey = (companyName) => {
+    parse = companyName.split(' ');
+    num = parse[1].split('.')
+    result = company.clients[num[0]-1]
+    
+    if (num[1]) {
+
+        for (i in num){
+
+            if (i>0){
+                console.log(num[i])
+                j = +num[i]
+                result = result.partners[j-1]
+            }
+        }
+    }
+
+    console.log(result);
+}
