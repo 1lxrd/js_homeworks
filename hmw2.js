94e231643c3d454ab8baabacba503173fa56e543
1lxrd <yashnik.dmitriu@gmail.com>
2023-12-13 19:36:17 +0200

    Second hmw

---
+++ hmw2.js
@@ -0,0 +1,34 @@
+mailTemp = /(^[a-zA-Z\d]+[.]?[a-zA-Z\d]*@)((gmail\.com)|(yahoo\.com))/;
+
+var arr = [
+    {
+        userName:"Test",
+        lastName:"Test",
+        email:"test.test@gmail.com"
+    },
+    {
+        userName:"Dmitro",
+        lastName:"Porohov",
+        email:"dmitro.porohov@yahoo.com"
+    },
+    {
+        userName:"Andrii",
+        lastName:"",
+        email:"andrii@mail.ru" // Нам такі не підходять
+    },
+];
+
+function filter (arr) {
+    let result = [];
+    arr.forEach(element => {
+        
+        let a = mailTemp.test(element.email);
+        if (a) result.push(element);
+
+    });
+
+    return result;
+}
+
+res = filter(arr);
+console.log(res);
